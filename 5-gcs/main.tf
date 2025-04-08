40244f7c5e7ecff9d96a1fb896d77bf86fbb148c
Michael Koenig <koenigmichael2002@gmail.com>
2025-04-08 10:54:18 +0200

    Add lifecycle to bucket

--- 5-gcs/main.tf
+++ 5-gcs/main.tf
@@ -9,6 +9,16 @@ resource "google_storage_bucket" "Bucket3" {
 
   uniform_bucket_level_access = true
 
+  lifecycle_rule {
+    condition {
+      age = 7
+    }
+    action {
+      type          = "SetStorageClass"
+      storage_class = "ARCHIVE"
+    }
+  }
+
 }
 
 resource "google_storage_bucket_object" "txt_file" {
